57087fac43352822aa7f63dd97e9d99120f150ec
Yun SeokMin <elraine@naver.com>
2016-09-01 17:03:52 +0900

    test2

---
+++ html/movie.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+<script language="javascript" src="/jquery/jquery-1.11.2.js"></script>
+<script language="javascript" src="/jquery/jquery-ui.js"></script>
+
+
+
+</head>
+<body>
+
+<h3>Hello</h3>
+
+<form action="#" method="GET">
+<input type="text" name="search">
+<input type="submit">
+
+</form>
+
+
+<?php
+	$client_id = "V9jj1_LsXcC1yH5_ORE2";
+	$client_secret = "UZXCf0pQgr";
+	$title = "터널";
+	$url = "https://openapi.naver.com/v1/search/movie.xml";
+	$is_post = true;
+	
+	/* 2016. 09. 01.  여기부터.
+	$fields = array('speaker'=>'mijin','speed'=>'0', 'text'=>$text);
+	$postvars = '';
+	foreach($fields as $key=>$value) {
+		$postvars .= $key . "=" . $value . "&";
+	}
+	*/
+	
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_POST, $is_post);
+	curl_setopt($ch, CURLOPT_POSTFIELDS,$postvars);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+	$headers = array();
+	$headers[] = "X-Naver-Client-Id: " . $client_id;
+	$headers[] = "X-Naver-Client-Secret: " . $client_secret;
+	 
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	 
+	$response = curl_exec ($ch);
+	curl_close ($ch);
+ 	
+	echo "<br>sdfasdf";
+	print_r($response);
+?>
+
+
+</body>
+</html>
+	
