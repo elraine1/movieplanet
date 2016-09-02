a26ec5f64b112b9ecf3176d98869b6b6ed0a75f2
Yun SeokMin <elraine@naver.com>
2016-09-02 16:44:49 +0900

    네이버 API 정상작동, 형태소분석기.

--- html/movie.php
+++ html/movie.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
 <script language="javascript" src="/jquery/jquery-1.11.2.js"></script>
@@ -13,48 +14,48 @@
 
 <h3>Hello</h3>
 
-<form action="#" method="GET">
-<input type="text" name="search">
+영화
+<form action="movie_proxy.php" method="POST">
+<input type="hidden" name="target" value="movie">
+<input type="text" name="query">
 <input type="submit">
+</form>
 
+뉴스
+<form action="movie_proxy.php" method="POST">
+<input type="hidden" name="target" value="news">
+<input type="text" name="query">
+<input type="submit">
 </form>
 
 
+
+
 <?php
+
 	$client_id = "V9jj1_LsXcC1yH5_ORE2";
 	$client_secret = "UZXCf0pQgr";
-	$target = "movie";
-	$url = "https://openapi.naver.com/v1/search/" . $target . ".xml";
-//	$url = "https://openapi.naver.com/v1/search/movie.xml";
-	$url = sprintf("%s?query=%s&display=5&start=1&sort=sim", $url, $query);
-	
-	$is_post = true;
-	
-	/* 2016. 09. 01.  여기부터.
-	$fields = array('speaker'=>'mijin','speed'=>'0', 'text'=>$text);
-	$postvars = '';
-	foreach($fields as $key=>$value) {
-		$postvars .= $key . "=" . $value . "&";
-	}
-	*/
-	
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_POST, $is_post);
-	curl_setopt($ch, CURLOPT_POSTFIELDS,$postvars);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-	$headers = array();
-	$headers[] = "X-Naver-Client-Id: " . $client_id;
-	$headers[] = "X-Naver-Client-Secret: " . $client_secret;
-	 
+	$query=urlencode("마트");
+	$display="5";
+	$start="1";
+	$is_post = true;
+	$output="xml" ;//json,xml
+
+	$url="https://openapi.naver.com/v1/search/local.xml";
+	$qry_str = "?&query=".$query."&start=".$start."&display".$display."&output".$output;
+	$headers = array("X-Naver-Client-Id: ".$client_id, "X-Naver-Client-Secret: ".$client_secret); //ClientID, SECRET
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url.$qry_str);
 	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//	curl_setopt($ch, CURLOPT_GET, $is_post);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	$response = curl_exec ($ch);
 	curl_close ($ch);
- 	
-	echo "<br>sdfasdf";
-	print_r($response);
+
+	echo $response;
 ?>
 
 
